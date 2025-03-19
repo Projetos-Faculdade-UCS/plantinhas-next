14544382b11b1a08fe29559e5c8356d874ba69c6
Bruno Martins <bgmartins@ucs.br>
2025-03-19 15:19:25 -0300

    feat: :sparkles: Estrutura inicial de Sign On

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,23 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-
+import { ApiClient } from "./shared/api-client";
  
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google
   ],
   callbacks: {
-          jwt(params) {
-            console.log("jwt", params);
-            return params;
-          },
+      jwt(params) {
+        const authApiUrl = process.env.AUTH_API_URL;
+        
+        ApiClient.getInstance().post(`${authApiUrl}/sign-on`, {}, {
+          headers:{
+            Authorization: `Bearer ${params.account?.access_token}`
+          }
+        })
+
+        return params;
+      },
     }
 })
