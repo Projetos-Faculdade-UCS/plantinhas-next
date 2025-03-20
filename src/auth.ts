5f8bc7325e5b7220ce048674cdb53ba806563b3d
Marco Haubrich <marhaubrich@ucs.br>
2025-03-20 11:34:26 -0300

    feat: :sparkles: Update JWT callback to handle missing id_token and adjust API endpoint

--- src/auth.ts
+++ src/auth.ts
@@ -1,23 +1,28 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { ApiClient } from "./shared/api-client";
- 
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google
   ],
   callbacks: {
-      jwt(params) {
-        const authApiUrl = process.env.AUTH_API_URL;
-        
-        ApiClient.getInstance().post(`${authApiUrl}/sign-on`, {}, {
-          headers:{
-            Authorization: `Bearer ${params.account?.access_token}`
-          }
-        })
+    jwt(params) {
+      const authApiUrl = process.env.AUTH_API_URL;
 
+      if (!params.account || !params.account.id_token) {
         return params;
-      },
-    }
+      }
+
+      ApiClient.getInstance().post(`${authApiUrl}/auth/api/v1/google/`, {}, {
+        headers: {
+          Authorization: `Bearer ${params.account.id_token}`,
+          "Content-Type": "application/json",
+        }
+      })
+
+      return params;
+    },
+  }
 })
