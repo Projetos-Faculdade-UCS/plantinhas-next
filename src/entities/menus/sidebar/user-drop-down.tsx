d81b48a986cd466d6a5da0d4c6e32de94df17bcf
Bruno Martins <bgmartins@ucs.br>
2025-07-07 21:49:16 -0300

    feat(user-drop-down): ✨ Refatora componente UserDropDown e adiciona opções de menu
    * Altera a lógica de importação para usar componentes do dropdown
    * Adiciona opções "Perfil", "Configurações" e "Sair" ao menu
    * Melhora a acessibilidade e a usabilidade do componente

--- src/entities/menus/sidebar/user-drop-down.tsx
+++ src/entities/menus/sidebar/user-drop-down.tsx
@@ -1,11 +1,10 @@
 import {
+    DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
+    DropdownMenuSeparator,
 } from '@/shared/ui/dropdown-menu';
-import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-} from '@radix-ui/react-dropdown-menu';
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 import { Session } from 'next-auth';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -17,7 +16,7 @@ type UserDropDownProps = {
 export async function UserDropDown({ session }: UserDropDownProps) {
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger>
+            <DropdownMenuTrigger className="mx-4 my-6 flex cursor-pointer items-center gap-3 rounded-sm px-2 py-2 outline-none">
                 <Image
                     src={session.user?.picture || ''}
                     alt="User Image"
@@ -30,13 +29,31 @@ export async function UserDropDown({ session }: UserDropDownProps) {
                     {session.user?.first_name} {session.user?.last_name}
                 </span>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem>
+            <DropdownMenuContent align="end" side="right" className="">
+                <DropdownMenuItem asChild>
                     <Link
                         href="/perfil"
-                        className="flex w-full items-center gap-3 px-4 py-6 hover:bg-gray-100"
+                        className="flex w-full cursor-pointer items-center"
                     >
+                        <i className="ph ph-user flex text-lg" />
                         Perfil
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="flex w-full cursor-pointer items-center">
+                    <i className="ph ph-gear flex text-lg" />
+                    Configurações
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                    asChild
+                    className="text-destructive flex w-full cursor-pointer items-center"
+                >
+                    <Link
+                        href="/signout"
+                        className="flex w-full cursor-pointer items-center"
+                    >
+                        <i className="ph ph-sign-out flex text-lg" />
+                        Sair
                     </Link>
                 </DropdownMenuItem>
             </DropdownMenuContent>
